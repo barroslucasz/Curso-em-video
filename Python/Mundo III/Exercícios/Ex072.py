d41ad18c2257a915280722799667448774c93292
Lucas Barros <bb-lucas@hotmail.com>
2023-06-23 12:55:21 -0300

    Update Ex072.py

--- Python/Mundo III/Exercícios/Ex072.py
+++ Python/Mundo III/Exercícios/Ex072.py
@@ -2,9 +2,20 @@ cont = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco',
         'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
         'doze', 'treze', 'quatorze', 'quinze', 'dezesseis',
          'dezessete', 'dezoito', 'dezenove', 'vinte')
+print('='*70)
+print('DESCUBRA COMO SE ESCREVE POR EXTENSO UM NÚMERO ENTRE 0 E 20')
+print('='*70)
 while True:
     numero = int(input('Digite um número entre 0 e 20: '))
     if 0 <= numero <= 20:
-        print(f'Você digitou o número {cont[numero]}')
+        print(f'O número {numero} por extenso é {cont[numero]}')
+        while True:
+            rep = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+            print('='*70)
+            if rep == 'N':
+                break
+            elif rep == 'S':
+                numero = int(input('Digite um número entre 0 e 20: '))
+                print(f'O número {numero} por extenso é {cont[numero]}')
         break
 print('--- FIM DO PROGRAMA ---')
