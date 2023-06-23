72dac765202a2934cfce42a16e533c4dc42c2f38
Lucas Barros <bb-lucas@hotmail.com>
2023-06-23 10:23:56 -0300

    Update Ex072.py

--- Python/Mundo III/Exercícios/Ex072.py
+++ Python/Mundo III/Exercícios/Ex072.py
@@ -6,7 +6,5 @@ while True:
     numero = int(input('Digite um número entre 0 e 20: '))
     if 0 <= numero <= 20:
         print(f'Você digitou o número {cont[numero]}')
-        dnv = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
-        if dnv == 'N':
-            break
+        break
 print('--- FIM DO PROGRAMA ---')
