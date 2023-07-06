3c5c78ae2fabefd287621a3c72fe539e722a0b44
Lucas Barros <bb-lucas@hotmail.com>
2023-07-06 07:28:18 -0300

    Update Ex094.py

--- Python/Mundo III/Exercícios/Ex094.py
+++ Python/Mundo III/Exercícios/Ex094.py
@@ -20,11 +20,19 @@ while True:
     if resp == 'N':
         break
 print('-='*20)
-print(f'Ao todo temos {len(galera)} pessoas cadastradas.')
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
 media = soma / len(galera)
-print(f'A média de idade é de {media:5.2f} anos.')
-print('As mulheres cadastradas foram ',end='')
+print(f'B) A média de idade é de {media:5.2f} anos.')
+print('C) As mulheres cadastradas foram ',end='')
 for p in galera:
     if p['sexo'] in 'Ff':
         print(f'{p["nome"]} ', end='')
 print()
+print('D) Lista de pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= media:
+        print('    ')
+        for k, v in p.items():
+            print(f'{k} = {v}; ',end='')
+        print()
+print('>>> Encerrado <<<')
