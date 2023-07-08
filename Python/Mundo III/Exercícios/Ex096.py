9ea0b0900a6694e3c772eecfe47056c3b28f9707
Lucas Barros <bb-lucas@hotmail.com>
2023-07-08 07:00:59 -0300

    atividades

--- Python/Mundo III/Exercícios/Ex096.py
+++ Python/Mundo III/Exercícios/Ex096.py
@@ -1,1 +1,10 @@
-# ihuuu
+def area (larg, compri):
+    a = larg * compri
+    print(f'A área de um terreno de {l}m X {c}m é de {a}m²')
+
+
+l = float(input('Largura: '))
+c = float(input('Comprimento: '))
+area(l,c)
+
+# Exercício realizado no dia 8deJul
---
+++ Python/Mundo III/Exercícios/Ex097.py
@@ -0,0 +1,9 @@
+def texto(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+texto('Lucas Barros')
+texto('Olá Mundo')
+texto('Oi meu nome é Lucas')
