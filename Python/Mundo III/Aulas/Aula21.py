f2ca789e0c6a0d4310b13d2ff95a845df112860e
Lucas Barros <bb-lucas@hotmail.com>
2023-07-12 08:20:26 -0300

    Update Aula21.py

---
+++ Python/Mundo III/Aulas/Aula21.py
@@ -0,0 +1,86 @@
+# >>>> INTERACTIVE HELP <<<<
+# >>>> DOCSTRINGS <<<<
+
+help(print)
+print(input.__doc__)
+
+def contador (i,f,p):
+    """
+    => SIGNIFICADO DOS PARAMETROS
+    -----------------------------
+
+    => parametro [i]: inicio
+    => parametro [f]: fim
+    => parametro [p]: passo
+
+    Função criada por Lucas
+    """
+    c = i
+    while c <= f:
+        print(f'{c} ', end='')
+        c += p
+    print('FIM!')
+
+contador(2,10,2)
+help(contador)
+print('-='*35)
+print()
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+# >>>> PARÂMETROS OPCIONAIS: <<<<
+
+def somar (a=0,b=0,c=0): # vai receber [a], vai receber [b] e o [c] vai ser opcional e poderia ser para os três parâmetros!
+    s = a+b+c
+    print(s)
+
+
+somar(1,2,3)
+somar(20,10)
+somar(10)
+somar()
+somar(a=5,c=2) # podemos nomear os parâmetros
+print('-='*35)
+print()
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+# >>>> ESCOPO DE VARIÁVEIS: <<<<
+
+def teste ():
+    x = 8 # variável local
+    print(f'Na função teste [n] vale {n}')
+    print(f'Na função teste [x] vale {x}')
+
+
+# programa principal:
+n = 2 # variável global
+print(f'No programa principal [n] vale {n}')
+teste()
+# print(f'No programa principal [x] vale {x}') # a variavel x só existe dentro do teste. sendo assim um escopo local
+print()
+print()
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+def funcao ():
+    global n1 # comando global torna o que está dentro da função em [GLOBAL]
+    n1 = 4 # variável local
+    print(f'[N1] local vale {n1}')
+
+
+n1 = 2 # variável global
+funcao()
+print(f'[N1] global vale {n1}')
+print('-='*35)
+print()
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+# >>>> RETORNO DE VALORES:  RETURN <<<<
+
+def soma (a=0,b=0,c=0):
+    s = a+b+c
+    return s
+
+
+r1 = soma(1,2,3)
+r2 = soma(1,2)
+r3 = soma(1)
+print(f'A soma dos valores é igual a {r1}, {r2} e {r3}')
