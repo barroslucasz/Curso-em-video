a2e3fdda37d8bd3388496a9c8b6279059d67c361
Lucas Barros <bb-lucas@hotmail.com>
2023-07-12 08:43:31 -0300

    Update Aula21.py

--- Python/Mundo III/Aulas/Aula21.py
+++ Python/Mundo III/Aulas/Aula21.py
@@ -84,3 +84,41 @@ r1 = soma(1,2,3)
 r2 = soma(1,2)
 r3 = soma(1)
 print(f'A soma dos valores é igual a {r1}, {r2} e {r3}')
+print('-='*35)
+print()
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+def fatorial(num = 1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é igual a {fatorial(n)}')
+print()
+
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+
+print(f'Os resultados são: {f1}, {f2} e {f3}')
+print()
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
+def par(num = 0):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+num = int(input('Digite um número: '))
+print(par(num))
+
+if par (num):
+    print('É Par!')
+else:
+    print('É Ímpar!')
