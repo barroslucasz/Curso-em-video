2f6beffe03e3b6952ee940a342cecdd894487379
Lucas Barros <bb-lucas@hotmail.com>
2023-07-13 09:25:57 -0300

    Update Ex101.py

--- Python/Mundo III/Exercícios/Ex101.py
+++ Python/Mundo III/Exercícios/Ex101.py
@@ -1,19 +1,14 @@
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA!'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL!'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO!'
+    
 
-def votacao (num=0):
-    idade = 2023 - n
-    if idade >= 18 < 70:
-        print(f'Com {idade} anos: VOTO OBRIGATÓRIO!')
-    elif idade == 16 or 17 :
-        print(f'Com {idade} anos: VOTO OPCIONAL!')
-    elif idade < 16:
-        print(f'Com {idade} anos: VOTO NEGADO!')
-    elif idade >= 70:
-        print(f'Com {idade} anos: VOTO OPCIONAL!')
-
-    return idade
-
-
-n = int(input('Digite seu ano de nascimento: '))
-votacao()
-
-# não consegui realizar esta atividade! amanhã corrijo com o guanabara!
+nasc = int(input('Em que ano você nasceu: '))
+print(voto(nasc))
