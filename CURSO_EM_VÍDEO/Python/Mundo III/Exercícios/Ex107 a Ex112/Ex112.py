94879e1639ae61f8096ea0ee28477d501f30a7d1
Lucas Barros <bb-lucas@hotmail.com>
2023-07-14 11:13:25 -0300

    Exercícios

--- CURSO_EM_VÍDEO/Python/Mundo III/Exercícios/Ex107 a Ex112/Ex112.py
+++ CURSO_EM_VÍDEO/Python/Mundo III/Exercícios/Ex107 a Ex112/Ex112.py
@@ -2,6 +2,6 @@ import moedas
 
 # programa principal
 p = float(input('Digite um preço R$: '))
-print(f'A metade de {p} é igual a R$: {moedas.metade(p)}')
-print(f'O dobro de {p} é igual a R$: {moedas.dobro(p)}')
-print(f'Aumentando 10%, temos R$: {moedas.aumentar(p, 10)}')
+print(f'A metade de {moedas.moeda(p)} é igual a {moedas.moeda(moedas.metade(p))}')
+print(f'O dobro de {moedas.moeda(p)} é igual a {moedas.moeda(moedas.dobro(p))}')
+print(f'Aumentando 10%, temos R$: {moedas.moeda(moedas.aumentar(p, 10))}')
