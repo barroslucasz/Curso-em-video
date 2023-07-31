8f7b4483c07598dbc4c426d850e14b1307dcd1b1
Lucas Barros <bb-lucas@hotmail.com>
2023-07-31 08:25:11 -0300

    atividades

---
+++ CURSO_EM_VÍDEO/Java/Aulas/EstruturasRepeticao/part2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cc = 0;
+        do {
+            System.out.println(cc);
+            cc++;
+
+        }while (cc <4);
+    }
+}
