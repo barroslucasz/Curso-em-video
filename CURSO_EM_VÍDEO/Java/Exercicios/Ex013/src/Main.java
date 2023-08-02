417437373500751517cd6419b64e740541276535
Lucas Barros <bb-lucas@hotmail.com>
2023-08-02 15:35:42 -0300

    atividades

--- CURSO_EM_VÍDEO/Java/Exercicios/Ex013/src/Main.java
+++ CURSO_EM_VÍDEO/Java/Exercicios/Ex013/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Salário do funcionário: ");
+        float salario = teclado.nextFloat();
+        double novosalario = salario + salario * 15 / 100;
+        System.out.println("Um funcionário que recebia "+salario+" com o aumento de 15% passará a receber "+novosalario);
     }
 }
