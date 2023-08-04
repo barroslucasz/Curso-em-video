681de35fd822db0bc1889e1a7f8bd6306f9f9ecc
Lucas Barros <bb-lucas@hotmail.com>
2023-08-04 18:11:23 -0300

    atividades

--- CURSO_EM_VÍDEO/Java/Exercicios/Ex015/src/Main.java
+++ CURSO_EM_VÍDEO/Java/Exercicios/Ex015/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Quantos dias alugado: ");
+        int dias = teclado.nextInt();
+        System.out.print("Quantos KM rodados: ");
+        float distancia = teclado.nextFloat();
+        int tdias = dias*60;
+        double kmr = distancia*0.15;
+        System.out.println("O total a pagar é R$: "+(tdias+kmr));
 
     }
 }
