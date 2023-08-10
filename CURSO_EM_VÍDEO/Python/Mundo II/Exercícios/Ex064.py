a8d29582c4cba0c06139d5230263096e6266a685
Lucas Barros <bb-lucas@hotmail.com>
2023-08-10 08:09:04 -0300

    Update Ex064.py

--- CURSO_EM_VÍDEO/Python/Mundo II/Exercícios/Ex064.py
+++ CURSO_EM_VÍDEO/Python/Mundo II/Exercícios/Ex064.py
@@ -1,8 +1,10 @@
-num = cont = soma = 0
+num = cont = soma = med = 0
 print('Digite [ 999 ] para parar.')
 num = int(input('Digite um número: '))
 while num != 999:
     soma += num
     cont += 1
+    med = soma/cont
     num = int(input('Digite um número: '))
-print('Você digitou {} números e a soma entre eles é {}.'.format(cont, soma))
+print('Você digitou {} a soma entre eles é {} e a média é {}.'.format(cont, soma, med))
+
